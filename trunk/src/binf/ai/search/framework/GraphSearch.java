d222301d5e420949995ac401fe9508f3115304ee
maartendb <maartendb@77cb14f4-d5da-11de-a7eb-d1396c3fe4e6>
2009-11-23 19:33:37 +0000

    GraphSearch proberen implementeren en daardoor dus ook closedList Niet zeker dat het correct is, maar wou gewoon iets doen :D

--- trunk/src/binf/ai/search/framework/GraphSearch.java
+++ trunk/src/binf/ai/search/framework/GraphSearch.java
@@ -3,32 +3,50 @@ package binf.ai.search.framework;
 import java.util.List;
 import binf.ai.search.nodestore.*;
 import binf.ai.search.problem.*;
+import java.util.ArrayList;
 
 public class GraphSearch implements Search {
 
-	private static int NO_DEPTH_LIMIT = -1;
-	private NodeStore openList;
-	private ClosedList closedList;
-	private int depthLimit;
+    private static int NO_DEPTH_LIMIT = -1;
+    private NodeStore openList;
+    private ClosedList closedList;
+    private int depthLimit;
 
-	public GraphSearch() {
-	}
-	
-	public GraphSearch(Problem problem, NodeStore openList,	ClosedList closedList) {
-		this.openList = openList;
-		this.closedList = closedList;
-		this.depthLimit = NO_DEPTH_LIMIT;
-	}
+    public GraphSearch() {
+    }
 
-	public GraphSearch(Problem problem, NodeStore openList,
-			ClosedList closedList, int depthLimit) {
-		this.openList = openList;
-		this.closedList = closedList;
-		this.depthLimit = depthLimit;
-	}
+    public GraphSearch(Problem problem, NodeStore openList, ClosedList closedList) {
+        this.openList = openList;
+        this.closedList = closedList;
+        this.depthLimit = NO_DEPTH_LIMIT;
+    }
 
-	public List<String> search(Problem problem) {
-		// implementeer hier de graph search
-		return null;
-	}
+    public GraphSearch(Problem problem, NodeStore openList,
+            ClosedList closedList, int depthLimit) {
+        this.openList = openList;
+        this.closedList = closedList;
+        this.depthLimit = depthLimit;
+    }
+
+    public List<String> search(Problem problem) {
+        // implementeer hier de graph search
+        Node root = new Node(problem.getInitialState());
+        openList.add(root);
+        while (!openList.isEmpty()) {
+            Node cur = openList.remove();
+            if (problem.isGoalState(cur.getState())) {
+                List<String> temp = new ArrayList<String>();
+                temp.add("Solution :\n" + cur.toString());
+                return temp;
+            }
+
+            if (!closedList.containsNode(cur)) {
+                closedList.add(cur);
+                for (Node child : NodeExpander.expandNode(problem, root)) {
+                    openList.add(child);
+                }
+            }
+        }
+        return null;
+    }
 }
--- trunk/src/binf/ai/search/nodestore/ClosedList.java
+++ trunk/src/binf/ai/search/nodestore/ClosedList.java
@@ -1,7 +1,40 @@
 package binf.ai.search.nodestore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ClosedList implements NodeStore {
+
+     private List<Node> list;
+
+    public ClosedList()
+    {
+        list = new ArrayList<Node>();
+    }
+
+    public void add(Node aNode) {
+        list.add(aNode);
+    }
+
+    public Node remove() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean containsNode(Node node)
+    {
+       return list.contains(node);
+
+    }
+
 
 	// implementatie van de closed list	
 }
