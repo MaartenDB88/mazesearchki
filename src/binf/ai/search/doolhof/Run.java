7296a9b5827925a15a9efa8b024b72331b807002
elias.de.coninck@gmail.com <elias.de.coninck@gmail.com@77cb14f4-d5da-11de-a7eb-d1396c3fe4e6>
2009-11-25 06:13:06 +0000

    time calculation

--- src/binf/ai/search/doolhof/Run.java
+++ src/binf/ai/search/doolhof/Run.java
@@ -27,6 +27,8 @@ public class Run {
     public static void main(String[] args) {
         GenereerDoolhof dh = new GenereerDoolhof();
         Problem problem = null;
+        long start;
+        long end;
 
         if (args.length != 0) {
             try {
@@ -47,45 +49,75 @@ public class Run {
         }
         dh.printDoolhof();
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Breedte eerst zoeken - treesearch");
         new BreadthFirstTreeSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Diepte zoeken met limiet 9 - treesearch");
         new DepthFirstTreeSearchWithLimit(problem, 9);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+//        start = System.currentTimeMillis();
 //        System.out.println("\n");
 //      Werkt niet correct zonder een limiet (Outofmemory error)
 //        System.out.println("Diepte eerst zoeken zonder limiet - treesearch");
 //        new DepthFirstTreeSearchNoLimit(problem);
+//        end = System.currentTimeMillis();
+//        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Kleinste kost eerst (priorityqueue) - treesearch");
         new PriorityQueueTreeSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Kleinste kost eerst (priorityqueue) - graphsearch");
         new PriorityQueueGraphSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Breedte eerst zoeken - graphsearch");
         new BreadthFirstGraphSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Diepte eerst zoeken met limit 9 - graphsearch");
         new DepthWithLimitGraphSearch(problem, 9);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("Diepte eerst zoeken zonder limiet - graphsearch");
         new DepthNoLimitGraphSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("AStarTreeSearch - treesearch");
         new AStarTreeSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
 
+        start = System.currentTimeMillis();
         System.out.println("\n");
         System.out.println("AStarGraphSearch - graphsearch");
         new AStarGraphSearch(problem);
+        end = System.currentTimeMillis();
+        System.out.println("Execution time was "+(end-start)+" ms.");
     }
 }
